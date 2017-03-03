b9d5773c33544a9e2b79c8bd5c5ca06cd8f85095
amo102 <a_modahl@hotmail.com>
2017-03-03 23:31:00 +0100

    Formatting

--- src/index.js
+++ src/index.js
@@ -7,18 +7,13 @@ import { createStore } from 'redux'
 import { Provider } from 'react-redux'
 import reducer from './reducers/ChessGame'
 
-const store = createStore(
-  reducer
-)
-
+const store = createStore(reducer)
 render(
   <Provider store={store}>
-
     <ChessGame myColor='White'
       playerName='Player1'
       opponentName='Player2'
       gameId={-1} />
   </Provider>,
-
-    document.getElementById('reactRoot')
-  )
+  document.getElementById('reactRoot')
+)
